990a276b57f2300edc468c8c4f2655017d1d04ff
Berk Subaşı <berksubasi@Berk-MacBook-Pro.local>
2025-03-16 02:12:08 +0300

    /tr prefix added;

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
-  // Varsayılan dil olarak Türkçe'yi ayarlıyoruz
   defaultLocale: 'tr',
   locales: ['tr', 'en'],
-  // Kullanıcının tarayıcı dilini algılayarak uygun dile yönlendirme
-  localeDetection: true
+  localeDetection: true,
+  localePrefix: 'always'
 });
 
 export const config = {
-  // Tüm yollar için middleware'i çalıştır, ancak bazı özel yolları hariç tut
   matcher: ['/((?!api|_next|.*\\..*|favicon.ico).*)']
 }; 
